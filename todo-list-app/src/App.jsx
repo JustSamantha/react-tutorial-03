6ef830c9bce6b76140881b18ff689994c6aa0f79
Samantha Castillo <samcastillogf@gmail.com>
2025-03-30 10:45:05 -0700

    - Adds basic structure for complete and incomplete todo lists and items

--- todo-list-app/src/App.jsx
+++ todo-list-app/src/App.jsx
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import './App.css'
 import TodoList from './TodoList'
 
 function App() {
+  const [completedTodos, setCompletedTodos] = useState([
+    { text: 'Take out the garbage', isCompleted: true},
+    { text: 'Make dinner', isCompleted: true}
+  ]);
+  const [incompletTodos, setinCompletTodos] = useState([
+    { text: 'Paint the house', isCompleted: false}
+  ]);
   return (
     <>
-    <TodoList completedTodos={[]} incompleteTodos={[]}/>
+    <TodoList completedTodos={completedTodos} incompleteTodos={incompletTodos}/>
     </>
   );
 }
