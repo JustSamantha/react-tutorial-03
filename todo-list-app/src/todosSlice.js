2601c270376542a7972f73d91c2fedf2da393351
Samantha Castillo <samanthacas@pm.me>
2025-04-01 10:05:47 -0700

    - Uses the payload action to get the text

--- todo-list-app/src/todosSlice.js
+++ todo-list-app/src/todosSlice.js
@@ -3,31 +3,38 @@ import { createSlice } from '@reduxjs/toolkit';
 export const todosSlice = createSlice({
   name: 'todos',
   initialState: {
-    value: [{
-      text: 'Go to the Store',
-      isCompleted: true,
-    }, {
-      text: 'New Todo',
-      isCompleted: false,
-    }],
-  },
-  reducers: {
-    createTodo: state => {
-      state.value = [...state.value, {
+    value: [
+      {
+        text: 'Go to the Store',
+        isCompleted: true,
+      },
+      {
         text: 'New Todo',
         isCompleted: false,
-      }];
+      },
+    ],
+  },
+  reducers: {
+    createTodo: (state) => {
+      state.value = [
+        ...state.value,
+        {
+          text: 'New Todo',
+          isCompleted: false,
+        },
+      ];
     },
-    markTodoAsCompleted: state => {
-      const text = 'New Todo';
-      const todo = state.value.find(t => t.text === text);
+    markTodoAsCompleted: (state, action) => {
+      const text = action.payload;
+      const todo = state.value.find((t) => t.text === text);
       todo.isCompleted = true;
     },
-    deleteTodo: state => {
-      const text = 'New Todo';
-      state.value = state.value.filter(t => t.text !== text);
+    deleteTodo: (state, action) => {
+      const text = action.payload;
+      state.value = state.value.filter((t) => t.text !== text);
     },
-  }
+  },
 });
 
-export const { createTodo, markTodoAsCompleted, deleteTodo } = todosSlice.actions;
+export const { createTodo, markTodoAsCompleted, deleteTodo } =
+  todosSlice.actions;
