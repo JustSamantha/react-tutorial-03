6bb6e80876e120345ebe50f87b004b6d4883d592
Samantha Castillo <samanthacas@pm.me>
2025-04-02 16:51:58 -0700

    - Implements the set todo to complete functionality

--- todo-list-app/src/thunks.js
+++ todo-list-app/src/thunks.js
@@ -4,9 +4,7 @@ import {
   loadingCompleted,
   loadingFailed,
 } from './loadingSlice';
-import {
-  todosUpdated,
-} from './todosSlice';
+import { todosUpdated } from './todosSlice';
 
 export const loadTodos = () => async (dispatch) => {
   dispatch(loadingStarted());
@@ -18,7 +16,7 @@ export const loadTodos = () => async (dispatch) => {
   } catch (e) {
     loadingFailed(e);
   }
-}
+};
 
 export const createTodo = (newTodoText) => async (dispatch, getState) => {
   try {
@@ -29,14 +27,29 @@ export const createTodo = (newTodoText) => async (dispatch, getState) => {
   } catch (e) {
     console.log(e);
   }
-}
+};
 
 export const deleteTodo = (todoId) => async (dispatch, getState) => {
   try {
     await axios.delete('/api/todos/' + todoId);
-    const updatedTodos = getState().todos.value.filter(t => t.id !== todoId);
+    const updatedTodos = getState().todos.value.filter((t) => t.id !== todoId);
     dispatch(todosUpdated(updatedTodos));
   } catch (e) {
     console.log(e);
   }
-}
+};
+
+export const markTodoAsCompleted = (todoId) => async (dispatch, getState) => {
+  try {
+    const response = await axios.put('/api/todos/' + todoId, {
+      isCompleted: true,
+    });
+    const updatedTodo = response.data;
+    const updatedTodos = getState().todos.value.map((t) =>
+      t.id === todoId ? updatedTodo : t
+    );
+    dispatch(todosUpdated(updatedTodos));
+  } catch (e) {
+    console.log(e);
+  }
+};
