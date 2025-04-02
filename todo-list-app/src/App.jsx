9032df2706d2585b3a4ebfcd3905eb95bc0976ae
Samantha Castillo <samcastillogf@gmail.com>
2025-04-01 17:39:29 -0700

    -Changes

--- todo-list-app/src/App.jsx
+++ todo-list-app/src/App.jsx
@@ -11,9 +11,27 @@ function App() {
     { text: 'Paint the house', isCompleted: false }
   ]);
 
+  function markTodoAsComplete(text) {
+    setIncompleteTodos(incompleteTodos.filter(t => t.text !== text));
+    setCompletedTodos([...completedTodos, { ...incompleteTodos.find(t => t.text === text), isCompleted: true }]);
+  }
+
+  function deleteTodo(text) {
+    setCompletedTodos(completedTodos.filter(t => t.text !== text));
+  }
+
+  function createTodo(text) {
+    setIncompleteTodos([...incompleteTodos, { text, isCompleted: false }]);
+  }
+
   return (
     <>
-    <TodoList completedTodos={completedTodos} incompleteTodos={incompleteTodos}/>
+    <TodoList
+      completedTodos={completedTodos}
+      incompleteTodos={incompleteTodos}
+      onCompletedClicked={markTodoAsComplete}
+      onDeleteClicked={deleteTodo}
+      onCreateClicked={createTodo} />
     </>
   );
 }
