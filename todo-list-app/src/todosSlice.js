ccffcd7dd609c1191b045c8774db0c7aa9ddfdfd
Samantha Castillo <samanthacas@pm.me>
2025-04-02 13:29:16 -0700

    -Adds a loading state

--- todo-list-app/src/todosSlice.js
+++ todo-list-app/src/todosSlice.js
@@ -1,33 +1,37 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { loadingCompleted } from './loadingSlice';
 
 export const todosSlice = createSlice({
   name: 'todos',
   initialState: {
-    value: [{
-      text: 'Go to the Store',
-      isCompleted: true,
-    }, {
-      text: 'New Todo',
-      isCompleted: false,
-    }],
+    value: [],
   },
   reducers: {
     createTodo: (state, action) => {
-      state.value = [...state.value, {
-        text: action.payload,
-        isCompleted: false,
-      }];
+      state.value = [
+        ...state.value,
+        {
+          text: action.payload,
+          isCompleted: false,
+        },
+      ];
     },
     markTodoAsCompleted: (state, action) => {
       const text = action.payload;
-      const todo = state.value.find(t => t.text === text);
+      const todo = state.value.find((t) => t.text === text);
       todo.isCompleted = true;
     },
     deleteTodo: (state, action) => {
       const text = action.payload;
-      state.value = state.value.filter(t => t.text !== text);
+      state.value = state.value.filter((t) => t.text !== text);
     },
-  }
+  },
+  extraReducers: (builder) => {
+    builder.addCase(loadingCompleted, (state, action) => {
+      state.value = action.payload;
+    });
+  },
 });
 
-export const { createTodo, markTodoAsCompleted, deleteTodo } = todosSlice.actions;
+export const { createTodo, markTodoAsCompleted, deleteTodo } =
+  todosSlice.actions;
--- todo-list-app/vite.config.js
+++ todo-list-app/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,9 +7,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://literate-tribble-r759v55r6j6cxpg6-3000.app.github.dev',
+        target: 'http://localhost:3000',
         changeOrigin: true,
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
