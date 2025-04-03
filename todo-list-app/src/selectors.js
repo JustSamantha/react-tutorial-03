16847dd9c948168ba1ad52a50cd4b449a83be5a8
Samantha Castillo <samanthacas@pm.me>
2025-04-03 08:36:26 -0700

    -Shows how to memoize selectors

--- todo-list-app/src/selectors.js
+++ todo-list-app/src/selectors.js
@@ -1,12 +1,11 @@
-export const getTodos = state => state.todos.value;
-export const getTodosLoading = state => !state.loading.value.completed;
+import { createSelector } from '@reduxjs/toolkit';
 
-export const getCompletedTodos = state => {
-  const todos = getTodos(state);
-  return todos.filter(t => t.isCompleted);
-}
+export const getTodos = (state) => state.todos.value;
+export const getTodosLoading = (state) => !state.loading.value.completed;
 
-export const getIncompleteTodos = state => {
-  const todos = getTodos(state);
-  return todos.filter(t => !t.isCompleted);
-}
+export const getCompletedTodos = createSelector([getTodos], (todos) =>
+  todos.filter((t) => t.isCompleted)
+);
+export const getIncompleteTodos = createSelector([getTodos], (todos) =>
+  todos.filter((t) => !t.isCompleted)
+);
