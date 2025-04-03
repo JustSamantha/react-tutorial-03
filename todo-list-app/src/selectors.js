0469830b036692349467f662086cc7ed975b6407
Samantha Castillo <samanthacas@pm.me>
2025-04-03 08:29:46 -0700

    -Shows how to use complex selectors

--- todo-list-app/src/selectors.js
+++ todo-list-app/src/selectors.js
@@ -1,3 +1,14 @@
-export const getTodos = state => state.todos.value;
+// Atomic selectors
+export const getTodos = (state) => state.todos.value;
+export const getTodosLoading = (state) => !state.loading.value.completed;
 
-export const getTodosLoading = state => !state.loading.value.completed;
+// Complex selectors
+export const getCompletedTodos = (state) => {
+  const todos = getTodos(state);
+  return todos.filter((t) => t.isCompleted);
+};
+
+export const getIncompletedTodos = (state) => {
+  const todos = getTodos(state);
+  return todos.filter((t) => !t.isCompleted);
+};
